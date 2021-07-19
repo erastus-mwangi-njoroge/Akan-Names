44b0c3b94979a49687ab6af10287614e6a0bcf9b
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-07-19 10:02:50 +0300

    add  global variables in js

---
+++ javascript/scripts.js
@@ -0,0 +1,3 @@
+var week_Days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
+var mNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var fNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
