34ab606c39d04f226d81ba69c8aade1ebd4729fa
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-07-19 10:06:39 +0300

    add variables for the  calculations

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,3 +2,8 @@ var week_Days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","
 var mNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var fNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 function calculateDayIndex(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.slice(0,2));
+    YY = parseInt(year.slice(2,4));
+    MM = parseInt(document.getElementById("month").value);
+    DD = parseInt(document.getElementById("date").value)
