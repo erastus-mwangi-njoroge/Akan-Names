f32d304f4df97f78699f58a49449d8379124236e
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-07-19 10:04:18 +0300

    add  function for calculating day of the week

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,4 @@
 var week_Days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
 var mNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var fNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+function calculateDayIndex(){
